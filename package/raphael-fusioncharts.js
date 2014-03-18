4b01b4a66ffd8195c33d0f6a2e2be34c039afee4
Shamasis Bhattacharya <shamasis@fusioncharts.com>
2014-03-19 02:18:07 +0530

    Version bump and packaging for release/1.0.6

--- package/raphael-fusioncharts.js
+++ package/raphael-fusioncharts.js
@@ -16,7 +16,7 @@ window.FusionCharts && window.FusionCharts.register('module', ['private', 'vendo
 
 
 /**!
- * RedRaphael 1.0.5 - JavaScript Vector Library
+ * RedRaphael 1.0.6 - JavaScript Vector Library
  * Copyright (c) 2012-2013 FusionCharts Technologies <http://www.fusioncharts.com>
  *
  * Raphael 2.1.0

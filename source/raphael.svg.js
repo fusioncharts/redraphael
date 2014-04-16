141bc6145a9647e4282fb13f4eaa7677cc4e6268
Shamasis Bhattacharya <shamasis@fusioncharts.com>
2014-04-16 13:07:33 +0530

    Replicated sync of stroke-opacity and rgba stroke (similar to the one between fill-opacity and rgba fill)

--- source/raphael.svg.js
+++ source/raphael.svg.js
@@ -748,8 +748,9 @@ window.Raphael && window.Raphael.svg && function(R) {
                                 $(node, {
                                     "fill-opacity": attrs["fill-opacity"]
                                 });
-                                o.gradient && updateGradientReference(o);
-                        } else if ((o.type == "circle" || o.type == "ellipse" || Str(value).charAt() != "r") && addGradientFill(o, value)) {
+                            o.gradient && updateGradientReference(o);
+                        }
+                        else if ((o.type == "circle" || o.type == "ellipse" || Str(value).charAt() != "r") && addGradientFill(o, value)) {
                             if ("opacity" in attrs || "fill-opacity" in attrs) {
                                 var gradient = R._g.doc.getElementById(node.getAttribute("fill").replace(/^url\(#|\)$/g, E));
                                 if (gradient) {
@@ -766,25 +767,37 @@ window.Raphael && window.Raphael.svg && function(R) {
                         if (clr[has]("opacity")) {
                             $(node, {
                                 "fill-opacity": (s.fillOpacity =
-                                        (clr.opacity > 1 ? clr.opacity / 100 : clr.opacity))
+                                    (clr.opacity > 1 ? clr.opacity / 100 : clr.opacity))
                             });
-                            o._.opacitydirty = true;
+                            o._.fillOpacityDirty = true;
                         }
-                        else if (o._.opacitydirty && R.is(attrs['fill-opacity'], "undefined") &&
+                        else if (o._.fillOpacityDirty && R.is(attrs['fill-opacity'], "undefined") &&
                                 R.is(params["fill-opacity"], "undefined")) {
                             node.removeAttribute('fill-opacity');
                             s.fillOpacity = E;
-                            delete o._.opacitydirty;
+                            delete o._.fillOpacityDirty;
                         }
                     case "stroke":
                         clr = R.getRGB(value);
                         node.setAttribute(att, clr.hex);
-                        att == "stroke" && clr[has]("opacity") && $(node, {
-                            "stroke-opacity": clr.opacity > 1 ? clr.opacity / 100 : clr.opacity
-                        });
-                        if (att == "stroke" && o._.arrows) {
-                            "startString" in o._.arrows && addArrow(o, o._.arrows.startString);
-                            "endString" in o._.arrows && addArrow(o, o._.arrows.endString, 1);
+                        if (att == "stroke") { // remove stroke opacity when stroke is set to none
+                            if (clr[has]("opacity")) {
+                                $(node, {
+                                    "stroke-opacity": (s.strokeOpacity =
+                                        (clr.opacity > 1 ? clr.opacity / 100 : clr.opacity))
+                                });
+                                o._.strokeOpacityDirty = true;
+                            }
+                            else if (o._.strokeOpacityDirty && R.is(attrs['stroke-opacity'], "undefined") &&
+                                    R.is(params["stroke-opacity"], "undefined")) {
+                                node.removeAttribute('stroke-opacity');
+                                s.strokeOpacity = E;
+                                delete o._.strokeOpacityDirty;
+                            }
+                            if (o._.arrows) {
+                                "startString" in o._.arrows && addArrow(o, o._.arrows.startString);
+                                "endString" in o._.arrows && addArrow(o, o._.arrows.endString, 1);
+                            }
                         }
                         break;
                     case "gradient":

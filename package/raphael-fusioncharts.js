14b41953407847c80de2a2e1bd52957cb7c60356
Rohit Kumar <rohit@fusioncharts.com>
2015-05-06 19:20:18 +0530

    Fix for Firefox 37.0.1 where stroke is not visible if the value of `stroke-dasha rray` is `none`.- Fix for this issue is the attribute `stroke-dasharray` will not be applied for the `none` value

--- package/raphael-fusioncharts.js
+++ package/raphael-fusioncharts.js
@@ -6857,6 +6857,15 @@ window.FusionCharts && window.FusionCharts.register('module', ['private', 'vendo
             oval: "M2.5,0A2.5,2.5,0,0,1,2.5,5 2.5,2.5,0,0,1,2.5,0z"
         },
         markerCounter = {},
+        // Due to a bug in Firefox if the value of "stroke-dasharray" attribute is "none"
+        // the stroke border will not be visible in Firefox 37.0.1
+        // To fix this issue the attribute "stroke-dasharray" will not be applied
+        // if the value is "none"
+        // Define an object that contains the property "none", this will be used for
+        // Comparision of "none" value
+        strokeValue = {
+            "none": true
+        },
         updateReferenceUrl = function () {
             return R._url = R._g.win.location.href.replace(/#.*?$/, E);
         };
@@ -7502,7 +7511,9 @@ window.FusionCharts && window.FusionCharts.register('module', ['private', 'vendo
                         }
                         node.setAttribute(att, value);
                         if (attrs["stroke-dasharray"]) {
-                            addDashes(o, attrs["stroke-dasharray"], params);
+                            // If the value of "stroke-dasharray" is "none" don't apply the attribute
+                            !strokeValue[attrs["stroke-dasharray"]] &&
+                                    addDashes(o, attrs["stroke-dasharray"], params);
                         }
                         if (o._.arrows) {
                             "startString" in o._.arrows && addArrow(o, o._.arrows.startString);
@@ -7510,7 +7521,8 @@ window.FusionCharts && window.FusionCharts.register('module', ['private', 'vendo
                         }
                         break;
                     case "stroke-dasharray":
-                        addDashes(o, value, params);
+                        // If the value of "stroke-dasharray" is "none" don't apply the attribute
+                        !strokeValue[value] && addDashes(o, value, params);
                         break;
                     case "fill":
                         var isURL = Str(value).match(R._ISURL);

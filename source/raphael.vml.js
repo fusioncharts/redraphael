f8a31e8a0260f9ff616276b92f636f8f35714ca9
AyanGhatak <ghatak.ayan.btech@gmail.com>
2016-05-20 09:10:19 +0530

    Add a generalised updateFollowers function to keep the followers in sync with the leader in case of VML supported browsers.
      - updateFollowers takes the context, callBack function name, and the other required arguments for the callback.
      - context(i.e. the element here) and the callBack nameSpaces(a String) are trimmed(shifted) off the main argument object.
      - Every indivual follower element contained in the leader element is made to invoke the callback function blindly with remaining arguments.

--- source/raphael.vml.js
+++ source/raphael.vml.js
@@ -483,6 +483,25 @@ window.Raphael && window.Raphael.vml && function(R) {
         }
     // res.paper.canvas.style.display = E;
     },
+    /*
+     * Keeps the follower element in sync with the leaders.
+     * First and second arguments represents the context(element) and the 
+     name of the callBack function respectively.
+     * The callBack is invoked for indivual follower Element with the rest of
+     arguments.
+    */
+    updateFollowers = R._updateFollowers = function () {
+        var i,
+            ii,
+            followerElem,
+            args = arguments,
+            o = arrayShift.call(args),
+            fnName = arrayShift.call(args);
+        for (i = 0, ii = o.followers.length; i < ii; i++) {
+            followerElem = o.followers[i].el;
+            followerElem[fnName].apply(followerElem, args);
+        }
+    },
     addGradientFill = function(o, gradient, fill) {
         o.attrs = o.attrs || {};
         var attrs = o.attrs,

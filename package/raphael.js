ebdc03dc15acdc591e9a0ea00eaef99feb1dabdd
Kingshuk Basak <kingshuk@fusioncharts.com>
2016-05-27 20:35:28 +0530

    Object linking deleted based on condition

--- package/raphael.js
+++ package/raphael.js
@@ -5440,8 +5440,10 @@
                 time = (e.initstatus * e.anim.top - e.prev) / (e.percent - e.prev) * ms;
                 e.status = e.initstatus;
                 delete e.initstatus;
-                delete e.el;
-                e.stop && animationElements.splice(l--, 1);
+                if (e.stop) {
+                    delete e.el;
+                    animationElements.splice(l--, 1);
+                }
             } else {
                 e.status = (e.prev + (e.percent - e.prev) * (time / ms)) / e.anim.top;
             }

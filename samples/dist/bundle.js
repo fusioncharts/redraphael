7c214b1f4b51352dee9f19b729088c37d401b235
Rohit Kumar <rohit.kr001@gmail.com>
2017-03-27 11:47:25 +0530

    Update sample

--- samples/dist/bundle.js
+++ samples/dist/bundle.js
@@ -12430,13 +12430,24 @@ var rect;
 Raphael(10, 10, 600, 400, function () {
     var paper = window.pap = this;
 
+    // Draw a red rectangle with red fill color.
     rect = paper.rect({
-        x: 10,
-        y: 10,
-        width: 300,
+        x: 0,
+        y: 0,
+        width: 500,
         height: 200
     }).attr({
         fill: '#FF0000'
+    });
+
+    // Draw text on top of rectangle with white fill color.
+    paper.text({
+        text: ['Use index.html as a template to work on temporary workspace!', '', 'Ensure that your test files are named spec-<somename>.html'],
+        x: 10,
+        y: 20,
+        fill: '#FFFFFF',
+        'text-anchor': 'left',
+        'vertical-align': 'top'
     });
 });
 

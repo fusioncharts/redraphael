3afa7690041b59c8a3b6a1cfdbd8833bb48fc8e0
Rohit Kumar <rohit.kr001@gmail.com>
2017-06-29 17:43:22 +0530

    RED-4151: Update modules field in package.json

--- source/index.js
+++ source/index.js
@@ -4,6 +4,7 @@ import svg from './raphael.svg';
 import vml from './raphael.vml';
 import canvas from './raphael.canvas';
 
-// export {svg, vml, canvas}
+export {svg, vml, canvas}
+
 export default Raphael
 

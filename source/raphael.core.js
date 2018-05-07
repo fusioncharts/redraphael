e6af785ee9be116744c7517f9a39d6fdf891d46c
Sumo <somedutta@fusioncharts.com>
2018-05-07 12:13:39 +0530

    RED-7444: removed pathToRelative from raphael.core

--- source/raphael.core.js
+++ source/raphael.core.js
@@ -1822,85 +1822,6 @@ var _win = (typeof window !== "undefined" ? window : typeof global !== "undefine
         res.toString = R._path2string;
         return res;
     },
-    pathToRelative = R._pathToRelative = function(pathArray) {
-        var pth = paths(pathArray);
-        if (pth.rel) {
-            return pathClone(pth.rel);
-        }
-        if (!R.is(pathArray, array) || !R.is(pathArray && pathArray[0], array)) { // rough assumption
-            pathArray = R.parsePathString(pathArray);
-        }
-        var res = [],
-        x = 0,
-        y = 0,
-        mx = 0,
-        my = 0,
-        start = 0;
-        if (pathArray[0][0] == "M") {
-            x = pathArray[0][1];
-            y = pathArray[0][2];
-            mx = x;
-            my = y;
-            start++;
-            res.push(["M", x, y]);
-        }
-        for (var i = start, ii = pathArray.length; i < ii; i++) {
-            var r = res[i] = [],
-            pa = pathArray[i];
-            if (pa[0] != lowerCase.call(pa[0])) {
-                r[0] = lowerCase.call(pa[0]);
-                switch (r[0]) {
-                    case "a":
-                        r[1] = pa[1];
-                        r[2] = pa[2];
-                        r[3] = pa[3];
-                        r[4] = pa[4];
-                        r[5] = pa[5];
-                        r[6] = +(pa[6] - x).toFixed(3);
-                        r[7] = +(pa[7] - y).toFixed(3);
-                        break;
-                    case "v":
-                        r[1] = +(pa[1] - y).toFixed(3);
-                        break;
-                    case "m":
-                        mx = pa[1];
-                        my = pa[2];
-                    default:
-                        for (var j = 1, jj = pa.length; j < jj; j++) {
-                            r[j] = +(pa[j] - ((j % 2) ? x : y)).toFixed(3);
-                        }
-                }
-            } else {
-                r = res[i] = [];
-                if (pa[0] == "m") {
-                    mx = pa[1] + x;
-                    my = pa[2] + y;
-                }
-                for (var k = 0, kk = pa.length; k < kk; k++) {
-                    res[i][k] = pa[k];
-                }
-            }
-            var len = res[i].length;
-            switch (res[i][0]) {
-                case "z":
-                    x = mx;
-                    y = my;
-                    break;
-                case "h":
-                    x += +res[i][len - 1];
-                    break;
-                case "v":
-                    y += +res[i][len - 1];
-                    break;
-                default:
-                    x += +res[i][len - 2];
-                    y += +res[i][len - 1];
-            }
-        }
-        res.toString = R._path2string;
-        pth.rel = pathClone(res);
-        return res;
-    },
     pathToAbsolute = R._pathToAbsolute = function(pathArray) {
         var pth = paths(pathArray), res;
         if (pth.abs) {
@@ -2590,18 +2511,6 @@ var _win = (typeof window !== "undefined" ? window : typeof global !== "undefine
         };
     };
 
-    /*\
-     * Raphael.pathToRelative
-     [ method ]
-     **
-     * Utility method
-     **
-     * Converts path to relative form
-     > Parameters
-     - pathString (string|array) path string or array of segments
-     = (array) array of segments.
-    \*/
-    R.pathToRelative = pathToRelative;
     R._engine = {};
 
     /*\

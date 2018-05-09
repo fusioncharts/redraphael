f9ba1b07fe3d2506b4ef60964b7a962d6d0e652d
kingshukbasak <kingshuk@fusioncharts.com>
2018-05-09 20:26:31 +0530

    RED-7140 : both touch and their counter-part mouse events are attached for all touch and hybrid devices

--- source/raphael.svg.js
+++ source/raphael.svg.js
@@ -1497,8 +1497,9 @@ export default function (R) {
         * Bind handler function for a particular event to Element
         * @param eventType - Type of event
         * @param handler - Function to be called on the firing of the event
+        * @param doNotModifyEvent - Boolean value that determines if the event has to be modified for touch devices
         \ */
-        elproto.on = function (eventType, handler) {
+        elproto.on = function (eventType, handler, doNotModifyEvent) {
             var elem = this,
                 node,
                 fn,
@@ -1520,7 +1521,7 @@ export default function (R) {
 
             fn = handler;
             oldEventType = eventType;
-            if (R.supportsTouch) {
+            if (R.supportsTouch && !doNotModifyEvent) {
                 eventType = R._touchMap[eventType] ||
                     (eventType === 'click' && 'touchstart') || eventType;
                 if (eventType !== oldEventType) {
@@ -1540,6 +1541,9 @@ export default function (R) {
                         newFn: fn,
                         newEvt: eventType
                     });
+                    // also attach the original event, mainly because of the
+                    // discrepancy in behaviour for hybrid devices.
+                    elem.on(oldEventType, handler, true);
                 }
             }
             if (this._ && this._.RefImg) {

9db50648339d35a89e5b5580b8e4909c1de3e724
Ashok Mandal <askipop@gmail.com>
2018-05-11 15:05:24 +0530

    RED-7478 : Minor changes related test cases and removal of repetetive testcases

--- tests/unit/raphael.lib.cacher-spec.js
+++ tests/unit/raphael.lib.cacher-spec.js
@@ -7,7 +7,7 @@ var addFunction = function(a, b){
   return a+b;
 };
 
-describe('Cacher basic functionality tests ', function(){
+describe('Cacher basic functionality tests ', function () {
   var cachedAddFunction, context;
 
   beforeEach(()=>{
@@ -16,7 +16,7 @@ describe('Cacher basic functionality tests ', function(){
     };
     cachedAddFunction = cacher(addFunction,context);
   });
-  it('should cache the result', function(){
+  it('should cache the result', function () {
     var resA, resB, resC;
     resA = cachedAddFunction(1,2);
     resB = cachedAddFunction(1,2);
@@ -26,7 +26,7 @@ describe('Cacher basic functionality tests ', function(){
     expect(resA === resC).toBe(true);
     expect(context.called).toBe(1);
   });
-  it('should behave same for null and empty string', function(){
+  it('should behave same for null and empty string', function () {
     var resA, resB, resC;
     resA = cachedAddFunction(null,'');
     resB = cachedAddFunction(null,'');
@@ -36,7 +36,7 @@ describe('Cacher basic functionality tests ', function(){
   });
 });
 
-describe('Cacher caching using limited cache size', function(){
+describe('Cacher caching using limited cache size', function () {
   var cachedAddFunction, context;
   beforeEach(() => {
     context = {
@@ -44,40 +44,12 @@ describe('Cacher caching using limited cache size', function(){
     };
     cachedAddFunction = cacher(addFunction, context, null, null, 10);
   });
-  it('should call the function after cache gets invalidated', function(){
+  it('should call the function after cache gets invalidated', function () {
     for(var i = 0; i <= 10; i++){
-      cachedAddFunction(i, i+1); 
+      cachedAddFunction(i, i + 1);
     }
     cachedAddFunction(0, 1);
     expect(context.called).toBe(12);
-  });
-  it('should invalidate a node', function(){
-    for(var i = 0; i < 10; i++){
-      cachedAddFunction(i , i+1);
-    }
-    expect(context.called).toBe(10);
-    cachedAddFunction(2, 3);
-    expect(context.called).toBe(10);
-    cachedAddFunction(3, 4);
-    expect(context.called).toBe(10);
-    cachedAddFunction(4, 5);
-    expect(context.called).toBe(10);
-    cachedAddFunction(5, 6);
-    expect(context.called).toBe(10);
-    cachedAddFunction(6, 7);
-    expect(context.called).toBe(10);
-    cachedAddFunction(7, 8);
-    expect(context.called).toBe(10);
-    cachedAddFunction(8, 9);
-    expect(context.called).toBe(10);
-    cachedAddFunction(9, 10);
-    expect(context.called).toBe(10);
-    cachedAddFunction(10, 11);
-    expect(context.called).toBe(11);
-    cachedAddFunction(11, 12);
-    expect(context.called).toBe(12);
-    cachedAddFunction(1,2);
-    expect(context.called).toBe(13);
   });
   it('should update the least recently used element properly', function(){
     var count = 0, res;
